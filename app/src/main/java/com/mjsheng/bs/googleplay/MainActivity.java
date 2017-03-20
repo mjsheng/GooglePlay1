7c4e2dc7e08bfda1dd9ef96359fde27f4993c932
mjsheng <501802639@qq.com>
2017-03-20 14:39:51 +0800

    02 ActionBar的显示   1. 继承ActionBarActivity   2. 清单文件改theme>Theme.AppCompat.Light 03 ActionBar 搜索   1. 在menu文件下的menu_main.xml添加   2.MainActivity的onCreateOptionsMenu()方法初始化SearchView   3.MainActivity继承OnQueryTextListener

---
+++ app/src/main/java/com/mjsheng/bs/googleplay/MainActivity.java
@@ -0,0 +1,55 @@
+package com.mjsheng.bs.googleplay;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity implements SearchView.OnQueryTextListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setLogo(R.mipmap.ic_launcher);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(getApplicationContext(), query, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Toast.makeText(getApplicationContext(), newText, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
